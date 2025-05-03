ece3a06d5397ab006a946564dfd544c2e00dac3e
rummansheikh69 <rummansheikh2004@gmail.com>
2025-05-03 16:12:10 +0600

    2nd day

--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -1,17 +1,48 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function App() {
-  let age = 3;
-  const address = 3;
+  // let age = 3;
+  // const address = 3;
 
-  const show = true;
+  // const show = true;
+
+  const [age, setAge] = useState(3);
+
+  const [address, setAddress] = useState("");
 
   return (
     <div>
       <div>
-        <h1>hello dsadsd</h1>
-        <h1>{age + address}</h1>
-        <h1>{!show ? "Labib" : "Rimu"}</h1>
+        <h1>hello world</h1>
+        {/* <h1>{age + address}</h1>
+        <h1>{!show ? "Labib" : "Rimu"}</h1> */}
+        <h1>{age}</h1>
+        <button
+          onClick={() => {
+            if (age < 10) {
+              setAge(age + 1);
+            }
+          }}
+        >
+          Click
+        </button>
+
+        <h1>{address}</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            prompt(address);
+          }}
+        >
+          <input
+            type="text"
+            value={address}
+            onChange={(e) => {
+              setAddress(e.target.value);
+            }}
+          />
+          <button>Submit</button>
+        </form>
       </div>
       <div>
         {/* <img src="./images/img1.jpg" alt="" title="Labib akta madarcod" />
